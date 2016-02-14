3a206f4d21bbaac162ebc341ffbbd0f7fb04580f
Jorge Capona <jcapona@gmail.com>
2016-02-14 11:32:30 -0300

    Added new routes

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (request, response) {
-  response.send("Hello World!");
+app.set('views',__dirname + '/views');
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/views/index.html');
 });
 
+app.get('/about', function (req, res) {
+  res.sendFile(__dirname+'/views/about.html');
+});
+
+app.get('/*', function (req, res) {
+  res.redirect('/');
+});
+	
 app.listen(5000, function(){
   console.log("app.js listening on port 5000")
 })
